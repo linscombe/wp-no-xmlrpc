6f21ab6f72da2bc56e383a84ffe7f66c88fb3ac2
linscombe <46058941+linscombe@users.noreply.github.com>
2025-08-22 07:03:24 -0500

    disable xmlrpc

---
+++ wp-no-xmlrpc.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This plugin will disable xmlrpc in Wordpress
+ *
+ * @package No XMLRPC
+ * @author Jason Linscombe
+ * @license GPL-3.0+
+ * @link https://github.com/linscombe/wp-no-xmlrpc
+ * @copyright 2025 Jason Linscombe All rights reserved.
+ *
+ *            @wordpress-plugin
+ *            Plugin Name: No XMLRPC
+ *            Plugin URI: https://github.com/linscombe/wp-no-xmlrpc
+ *            Description: This plugin will disable xmlrpc in Wordpress
+ *            Version: 1.0
+ *            Author: Jason Linscombe
+ *            Author URI: https://github.com/linscombe
+ *            Text Domain: wp-no-xmlrpc
+ *            Contributors: Jason Linscombe
+ *            License: GPL-3.0+
+ *            License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+add_filter( 'xmlrpc_enabled', '__return_false' );
+
+?>
